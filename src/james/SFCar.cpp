2dbf8a40f2f8aaf7a9a86f84b09be2ca33b27ac3
James Peters <32214004+JamesPeters86@users.noreply.github.com>
2017-10-30 17:35:02 -0600

    Added SFCar dummy cpp and h

---
+++ src/james/SFCar.cpp
@@ -0,0 +1,17 @@
+/*
+ * SFCar.cpp
+ *	Dummy class for compilability
+ *  Created on: Oct 21, 2017
+ *      Author: james
+ */
+
+#include "SFCar.h"
+
+SFCar::SFCar() {
+	// TODO Auto-generated constructor stub
+}
+
+SFCar::~SFCar() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/james/SFCar.h
@@ -0,0 +1,21 @@
+/*
+ * SFCar.h
+ *	Dummy header for compilability
+ *  Created on: Oct 21, 2017
+ *      Author: james
+ */
+
+#ifndef SRC_JAMES_SFCAR_H_
+#define SRC_JAMES_SFCAR_H_
+
+class SFCar
+{
+public:
+	SFCar();
+	virtual ~SFCar();
+
+private:
+
+};
+
+#endif /* SRC_JAMES_SFCAR_H_ */
